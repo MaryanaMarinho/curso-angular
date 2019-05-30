d0bb5d2664faf09af33498d5aeb04bb123a29ca5
Maryana Marinho <maryanalirius@hotmail.com>
2019-05-30 18:44:40 -0300

    Introducao aos Templates

--- primeiro-projeto/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ primeiro-projeto/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+   }
 
   ngOnInit() {
   }
