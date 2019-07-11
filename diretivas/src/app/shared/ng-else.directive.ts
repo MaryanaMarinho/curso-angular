2f7b77fa6763f46f7e70ecc92a7b8e3a722a04c8
Maryana Marinho <maryanalirius@hotmail.com>
2019-07-11 15:28:19 -0300

    Criando uma diretiva de estrutura (ngElse)

---
+++ diretivas/src/app/shared/ng-else.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[ngElse]'
+})
+export class NgElseDirective {
+
+  @Input() set ngElse(condition: boolean) {
+    if (!condition) {
+      // é assim que renderizamos um template numa diretiva estrutural
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    }
+    else {
+      this.viewContainerRef.clear();
+    }
+  }
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef
+  ) { }
+
+}
