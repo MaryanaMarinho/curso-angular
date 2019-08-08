ef4d965b004597aaf0633896b31d6348f64c4e90
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-08 01:38:51 -0300

    Criando um modulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,34 +1,38 @@
+import { FormsModule } from '@angular/forms';
 import 'materialize-css';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 //import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+/* import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component'; */
 import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent,
     HomeComponent,
     LoginComponent,
+    /* CursosComponent,
     CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    CursoNaoEncontradoComponent */
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     MaterializeModule,
+    CursosModule,
     AppRoutingModule
     //routing
   ],
-  providers: [CursosService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
