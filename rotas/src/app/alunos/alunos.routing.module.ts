7bb29ba5fc90710188d5e8c9e10a093063b5ff98
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-09 11:47:18 -0300

    Rotas Filhas

---
+++ rotas/src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,22 @@
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunosComponent } from './alunos.component';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const alunsoRoutes = [
+  {path: 'alunos', component: AlunosComponent, children : [
+    {path: 'novo', component: AlunoFormComponent},
+    {path: ':id', component: AlunoDetalheComponent},
+    {path: ':id/editar', component: AlunoFormComponent}
+
+  ]}
+
+];
+
+@NgModule({
+	imports: [RouterModule.forChild(alunsoRoutes)],
+	exports: [RouterModule]
+})
+
+export class AlunosRoutingModule {}
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component'; */
 import { AppRoutingModule } from './app.routing.module';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { CursosModule } from './cursos/cursos.module';
     FormsModule,
     MaterializeModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
     //routing
   ],
