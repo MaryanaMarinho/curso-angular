94bf26cc58a3f614aff536f17591caa0d5cee5aa
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-14 14:30:05 -0300

    Definindo rota padrao e wildcard (rota nao encontrada)

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 /* import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component'; */
@@ -37,10 +38,14 @@ const appRoutes: Routes = [
   //{ path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
 
   {
-    path: '',
+    path: 'home',
     component: HomeComponent,
     canActivate: [AuthGuard]
-  }
+  },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: '**', component: PaginaNaoEncontradaComponent } // canActivate: [AuthGuard] voltaria para a pagina de login se nao encontrasse a rota que o usuario digitou
 ];
 
 @NgModule({
