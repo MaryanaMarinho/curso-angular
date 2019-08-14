c6aeb847c5ce7a5fbb7c3a7f61d0e0b03d8fd35a
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-14 13:22:01 -0300

    Usando Guarda de Rotas: CanDeactivate com Interface Generica

--- rotas/src/app/guards/alunos-deactivate.guard.ts
+++ rotas/src/app/guards/alunos-deactivate.guard.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AlunoFormComponent } from '../alunos/aluno-form/aluno-form.component';
+import { IFormCanDeactivate } from './iform-candeactivate';
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
 
     canDeactivate(
-      component: AlunoFormComponent,
+      component: IFormCanDeactivate,
       route: ActivatedRouteSnapshot,
       state: RouterStateSnapshot
     ): Observable<boolean>|Promise<boolean>|boolean {
 
       console.log('guarda de desativação');
 
-      return component.podeMudarRota();
+      //return component.podeMudarRota();
+
+      return component.podeDesativar();
   }
 }
 
