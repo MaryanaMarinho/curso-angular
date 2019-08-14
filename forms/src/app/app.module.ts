360b361e23de5a21689c6b45523b750de6a5586e
Maryana Marinho <maryanalirius@hotmail.com>
2019-08-14 16:03:55 -0300

    Forms (template driven) Controles ngForm, ngSubmit e ngModel

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -13,6 +14,7 @@ import { DataFormComponent } from './data-form/data-form.component';
     DataFormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
