20e30a749027305217c6d9569f64d977e3d29b1a
Maryana Marinho <maryanalirius@hotmail.com>
2019-10-30 23:21:49 -0300

    Forms (template driven) Pesquisando endereço automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
+import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -20,7 +22,7 @@ export class TemplateFormComponent implements OnInit {
 
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -34,6 +36,29 @@ export class TemplateFormComponent implements OnInit {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo),
       'is-invalid': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultaCEP(cep) {
+
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if(cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+        .subscribe(dados => {
+          console.log(dados);
+
+        });
+      }
     }
   }
 
