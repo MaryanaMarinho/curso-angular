9193eeeab128b2a72b7c41c775b307cd0c19f491
Maryana Marinho <maryanalirius@hotmail.com>
2019-11-03 12:12:19 -0300

    Formulários reativos: Configuração (Módulo e Componente)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { FormsModule } from '@angular/forms';
+import { DataFormModule } from './data-form/data-form.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent,
-
+    AppComponent
   ],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    TemplateFormModule
+    TemplateFormModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ forms/src/app/data-form/data-form.module.ts
@@ -0,0 +1,15 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { DataFormComponent } from './data-form.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    DataFormComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class DataFormModule { }
