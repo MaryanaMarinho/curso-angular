65803247e57cf97449c98259385ea4d89b9e7ce0
Maryana Marinho <maryanalirius@hotmail.com>
2019-11-17 12:50:41 -0300

    Formulários: Criando um serviço de Estados Brasileiros

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,3 +1,5 @@
+import { EstadoBr } from './../shared/models/estado-br';
+import { DropdownService } from './../shared/services/dropdown.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -10,12 +12,20 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
+  estados: EstadoBr[];
 
   constructor(
     private formBuilder: FormBuilder,
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private dropdownService: DropdownService) { }
 
   ngOnInit() {
+
+    this.dropdownService.getEstadosBr().subscribe(dados => {
+      this.estados = dados;
+      console.log(dados);
+
+    })
 
     /* this.formulario = new FormGroup({
       nome: new FormControl(null),
---
+++ forms/src/app/shared/models/estado-br.ts
@@ -0,0 +1,5 @@
+export interface EstadoBr {
+  id: number;
+  sigla: String;
+  nome: String;
+}
